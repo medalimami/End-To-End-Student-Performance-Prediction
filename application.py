dc87e136ce8a2ff4510e481fac89da2614b4c749
medalimami <dalymami741@outlook.com>
2025-10-03 22:10:49 -0400

    config update

--- application.py
+++ application.py
@@ -4,13 +4,13 @@ import pandas as pd
 
 from pipeline.predict_pipeline import CustomData, PredictPipeline
 
-app = Flask(__name__)
+application = Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def index():
     return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@application.route('/predictdata',methods=['GET','POST'])
 def predict_datapoint():
     if request.method == 'GET':
         return render_template('home.html')
@@ -33,6 +33,6 @@ def predict_datapoint():
         return render_template("home.html",results=results[0])
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    application.run(host="0.0.0.0")
 
 
